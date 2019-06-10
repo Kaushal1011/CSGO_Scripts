c325df14064df6354f08f9cbbdb6d980aec3bf3b
kaushal1011 <kaushalpatil10@gmail.com>
2019-06-11 02:00:32 +0530

    first script

---
+++ newscript.py
@@ -0,0 +1,54 @@
+import keyboard
+import pymem
+import pymem.process
+import pyautogui
+import time
+
+dwEntityList = (0x4D03AA4)
+dwForceAttack = (0x313519C)
+dwLocalPlayer = (0xCF1A4C)
+m_fFlags = (0x104)
+m_iCrosshairId = (0xB3AC)
+m_iTeamNum = (0xF4)
+
+pm = pymem.Pymem("csgo.exe")
+client = pymem.process.module_from_name(pm.process_handle, "client_panorama.dll").lpBaseOfDll
+
+
+def main():
+    
+    while True:
+        player = pm.read_int(client + dwLocalPlayer)
+
+        
+        entity = pm.read_int(player + m_iCrosshairId)
+        
+             
+
+        if entity > 0 and entity <= 64:
+            entity = pm.read_int(client + dwEntityList + (entity -1) * 0x10)
+            entity_team = pm.read_int(entity + m_iTeamNum)
+            player_team = pm.read_int(player + m_iTeamNum)
+                    
+            if player_team != entity_team:
+                if keyboard.is_pressed("left shift") or keyboard.is_pressed("left ctrl"):
+                    pyautogui.click()
+                    time.sleep(0.015)
+
+                # shooting = True
+                # pm.write_int(client + dwForceAttack, 5)
+        
+            
+        # if not keyboard.is_pressed(trigger_key) and shooting == True:
+        #     pm.write_int(client + dwForceAttack, 4)
+        #     shooting = False
+
+def a():
+    main()
+
+if __name__ == '__main__':
+    a()
+
+    
+            
+            
